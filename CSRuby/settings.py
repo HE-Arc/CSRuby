52ddf50f3e0f849ce50d577f80e2d668ce1fc339
artan.sadiku <artan.sadiku@he-arc.ch>
2020-02-13 17:36:52 +0100

    Added base app and view.

--- CSRuby/settings.py
+++ CSRuby/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'CSRuby.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "csruby_app/templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
