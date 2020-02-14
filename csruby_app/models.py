26b20f1c1738de59f257313552ff0a839dbff6f9
artan.sadiku <artan.sadiku@he-arc.ch>
2020-02-14 16:44:19 +0100

    Added example of react code

--- csruby_app/models.py
+++ csruby_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
--- csruby_app/views.py
+++ csruby_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render, redirect
+from .models import Users
+from .serializers import UsersSerializer
+from rest_framework import generics
 
 # Create your views here.
+class UsersListCreate(generics.ListCreateAPIView):
+    queryset = Users.objects.all()
+    serializer_class = UsersSerializer
