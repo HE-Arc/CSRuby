8bdf490871afa79e3eb000dafcb944084dc79e54
artan.sadiku <artan.sadiku@he-arc.ch>
2020-02-27 22:17:47 +0100

    Added database password to env. var and set default value

--- CSRuby/settings/common_settings.py
+++ CSRuby/settings/common_settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'csruby_database',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': os.environ['DATABASE_PASSWORD'],
         'HOST': '127.0.0.1',
         'PORT': '3306',
     },
