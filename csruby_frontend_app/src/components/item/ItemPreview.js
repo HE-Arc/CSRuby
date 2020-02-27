3e32d8e54bfae41b9895e80c54d69b383e2b683d
Minnig Nicolas <nicolas.minnig@he-arc.ch>
2020-02-27 14:00:23 +0100

    added basic item preview component

---
+++ csruby_frontend_app/src/components/item/ItemPreview.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+class ItemPreview extends Component {
+  render(){
+    return (
+      <div>
+          <img src="/static/images/stattrack_survival_knife.png" alt="StatTrack Survival Knife"/>
+          <h1>★ StatTrak™ Survival Knife | Fade</h1>
+          <p>Selling price 1758.58$</p>
+      </div>
+    );
+  }
+}
