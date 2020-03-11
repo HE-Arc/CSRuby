340a3498e08ad02ba30948c7292a96de57c81638
artan.sadiku <artan.sadiku@he-arc.ch>
2020-03-11 09:35:58 +0100

    Changed class to className

--- csruby_frontend_app/src/components/Dashboard.js
+++ csruby_frontend_app/src/components/Dashboard.js
@@ -3,27 +3,27 @@ import React, { Component } from "react";
 class Dashboard extends Component {
   render() {
     return (
-      <div class="content text-light mt-5">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg p-3">
-              <div class="csruby-bg-darkest text-center">
-                <img src="/static/csruby_frontend_app/images/m4a4_howl.png" class="img-fluid text-center" alt="Responsive image" />
+      <div className="content text-light mt-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg p-3">
+              <div className="csruby-bg-darkest text-center">
+                <img src="/static/csruby_frontend_app/images/m4a4_howl.png" className="img-fluid text-center" alt="Responsive image" />
               </div>
-              <div class="row mt-3">
-                <div class="col-4">
-                  <button type="button" class="btn btn-lg btn-block csruby-bg-red">Buy</button>
+              <div className="row mt-3">
+                <div className="col-4">
+                  <button type="button" className="btn btn-lg btn-block csruby-bg-red">Buy</button>
                 </div>
-                <div class="col-4">
-                  <button type="button" class="btn btn-lg btn-block csruby-bg-red">Sell</button>
+                <div className="col-4">
+                  <button type="button" className="btn btn-lg btn-block csruby-bg-red">Sell</button>
                 </div>
-                <div class="col-4">
-                  <button type="button" class="btn btn-lg btn-block csruby-bg-red">Favourite</button>
+                <div className="col-4">
+                  <button type="button" className="btn btn-lg btn-block csruby-bg-red">Favourite</button>
                 </div>
               </div>
             </div>
-            <div class="col-lg p-3">
-              <div class="csruby-bg-darkest csruby-height-100">
+            <div className="col-lg p-3">
+              <div className="csruby-bg-darkest csruby-height-100">
                 <p>graph</p>
               </div>
             </div>
