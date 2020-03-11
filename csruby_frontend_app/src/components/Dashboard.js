83a4d501b98b7db8453c7aefd9dc12d7e7b956b3
artan.sadiku <artan.sadiku@he-arc.ch>
2020-03-11 09:29:57 +0100

    Added chart.js and added missing dashboard modifications

--- csruby_frontend_app/src/components/Dashboard.js
+++ csruby_frontend_app/src/components/Dashboard.js
@@ -3,22 +3,33 @@ import React, { Component } from "react";
 class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <div>
-          <p>item image</p>
-          <p>Buy</p>
-          <p>Sell</p>
-          <p>Favourite</p>
-        </div>
-        <div>
-          <p>graph</p>
-        </div>
-        <div>
-          <p>Item info</p>
-        </div>
-        <div>
-          <p>Sellers</p>
-          <p>Buyers</p>
+      <div class="content text-light mt-5">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg p-3">
+              <div class="csruby-bg-darkest text-center">
+                <img src="/static/csruby_frontend_app/images/m4a4_howl.png" class="img-fluid text-center" alt="Responsive image" />
+              </div>
+              <div class="row mt-3">
+                <div class="col-4">
+                  <button type="button" class="btn btn-lg btn-block csruby-bg-red">Buy</button>
+                </div>
+                <div class="col-4">
+                  <button type="button" class="btn btn-lg btn-block csruby-bg-red">Sell</button>
+                </div>
+                <div class="col-4">
+                  <button type="button" class="btn btn-lg btn-block csruby-bg-red">Favourite</button>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg p-3">
+              <div class="csruby-bg-darkest csruby-height-100">
+                <p>graph</p>
+              </div>
+            </div>
+          </div>
+          <h2>Item information</h2>
+          <h2>Buyers | Sellers</h2>
         </div>
       </div>
     );
