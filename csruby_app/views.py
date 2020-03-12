8fdd609827f8db3d3261330c0a6ed576bdfab2d4
nicopraz <nicolas.praz@he-arc.ch>
2020-03-12 14:49:18 +0100

    added user inscription

--- csruby_app/views.py
+++ csruby_app/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render, redirect
 from .models import User
 from .serializers import UserSerializer
-from rest_framework import generics
+from .serializers import UserRegistrationSerializer
+from rest_framework import generics, permissions
 
 # Create your views here.
 class UserListCreate(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [
+        permissions.AllowAny
+    ]
+
+    def create(self, request, *args, **kwargs):
+        serializer = UserRegistrationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        # Everything's valid, so send it to the UserSerializer
+        model_serializer = UserSerializer(data=serializer.data)
+        model_serializer.is_valid(raise_exception=True)
+        model_serializer.save()
+
+        return Response(model_serializer.data)
--- csruby_frontend_app/src/components/Signup.js
+++ csruby_frontend_app/src/components/Signup.js
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Signup extends Component {
   constructor(props) {
     super(props);
     this.state={
-      username:'',
+      profilename:'',
       email:'',
       password:'',
-      confirmPassword:''
+      confirm_password:''
     }
 
     this.handle_change = this.handle_change.bind(this)
-    this.log_values = this.log_values.bind(this)
+    this.submit_form = this.submit_form.bind(this)
   };
 
   handle_change(event){
@@ -24,55 +25,85 @@ class Signup extends Component {
 
   };
 
-  submitForm(event){
-    console.log(this.state.username);
-    console.log(this.state.email);
-    console.log(this.state.password);
-    console.log(this.state.confirmPassword);
+  submit_form(event){
+    // console.log(this.state.profilename);
+    // console.log(this.state.email);
+    // console.log(this.state.password);
+    // console.log(this.state.confirm_password);
+
+    var userFormData = new FormData();
+    userFormData.append('profilename', this.state.profilename);
+    userFormData.append('email', this.state.email);
+    userFormData.append('password', this.state.password);
+    userFormData.append('confirm_password', this.state.confirm_password);
+    userFormData.append('steamid', '1');
+
+    axios.defaults.xsrfCookieName = 'csrftoken'
+    axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+
+    axios({
+      method: 'post',
+      url: '/api/user/',
+      data: userFormData
+      })
+      .then(function(response){
+        console.log("ok");
+        console.log(response);
+      })
+      .catch(function(error){
+        console.log("error");
+        console.log(error);
+        console.log(error.response.data);
+        event.preventDefault();
+      });
+
+      event.preventDefault();
+
   };
 
   render() {
     return (
-      <div>
-        <input
-          type="text"
-          name="username"
-          placeholder="Enter Username"
-          value={this.state.username}
-          onChange={this.handle_change}
-        />
-        <br/>
-        <input
-          type="text"
-          name="email"
-          placeholder="Enter email"
-          value={this.state.email}
-          onChange={this.handle_change}
-        />
-        <br/>
-        <input
-          type="password"
-          name="password"
-          placeholder="Enter Password"
-          value={this.state.password}
-          onChange={this.handle_change}
-        />
-        <br/>
-        <input
-          type="password"
-          name="confirmPassword"
-          placeholder="Confirm Password"
-          value={this.state.confirmPassword}
-          onChange={this.handle_change}
-        />
-        <br/>
-        <button
-          type="submit"
-          onClick={this.submitForm}
-        >
-        Signup
-        </button>
-      </div>
+      <form onSubmit={this.submit_form}>
+        <div>
+          <input
+            type="text"
+            name="profilename"
+            placeholder="Enter Profilename"
+            value={this.state.profilename}
+            onChange={this.handle_change}
+          />
+          <br/>
+          <input
+            type="text"
+            name="email"
+            placeholder="Enter email"
+            value={this.state.email}
+            onChange={this.handle_change}
+          />
+          <br/>
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter Password"
+            value={this.state.password}
+            onChange={this.handle_change}
+          />
+          <br/>
+          <input
+            type="password"
+            name="confirm_password"
+            placeholder="Confirm Password"
+            value={this.state.confirm_password}
+            onChange={this.handle_change}
+          />
+          <br/>
+          <button
+            type="submit"
+          >
+          Signup
+          </button>
+        </div>
+      </form>
     )
   };
 }
