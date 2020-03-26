b56211dce0ce065d985ad89d526f42df18660489
artan.sadiku <artan.sadiku@he-arc.ch>
2020-03-26 11:24:13 +0100

    Fixed unit tests for models

--- csruby_app/tests/test_models.py
+++ csruby_app/tests/test_models.py
@@ -1,33 +1,26 @@
-from datetime import datetime
 from django.test import TestCase
 from csruby_app.models import *
+from django.utils import timezone
+import pytz
 
 class UserAndRoleTestCase(TestCase):
     """This class is used to test the User and Role model using django.test package"""
 
     def setUp(self):
-        self.user = User.objects.create(email="user@domain.com", password="securepassword", profilename="user", steamid="61772925218070718")
-        self.user_steamdid_bad = User.objects.create(email="user_bad@domain.com", password="securepassword", profilename="user", steamid="1")
+        self.user = CSRuby_User.objects.create(email="user@domain.com", password="securepassword", username="user", steamid="61772925218070718")
+        self.user_steamdid_bad = CSRuby_User.objects.create(email="user_bad@domain.com", password="securepassword", username="user", steamid="1")
 
-        self.role_user = Role.objects.create(name="User")
-
-    def test_user_with_role(self):
-        self.user.role = self.role_user
-        self.assertTrue(isinstance(self.user, User))
-        self.assertTrue(isinstance(self.user.role, Role))
+    def test_user(self):
+        self.assertTrue(isinstance(self.user, CSRuby_User))
         self.assertTrue(isinstance(self.user.email, str))
         self.assertTrue(isinstance(self.user.password, str))
-        self.assertTrue(isinstance(self.user.profilename, str))
+        self.assertTrue(isinstance(self.user.username, str))
         self.assertTrue(isinstance(self.user.steamid, str))
         self.assertEqual(len(self.user.steamid), 17)
 
     def test_user_steamid_bad(self):
-        self.assertTrue(isinstance(self.user_steamdid_bad, User))
+        self.assertTrue(isinstance(self.user_steamdid_bad, CSRuby_User))
         self.assertNotEqual(len(self.user_steamdid_bad.steamid), 17)
-
-    def test_role(self):
-        self.assertTrue(isinstance(self.role_user, Role))
-        self.assertTrue(isinstance(self.role_user.name, str))
 
 class ItemTestCase(TestCase):
     """This class is used to test the Item model using django.test package"""
@@ -45,13 +38,14 @@ class PriceTestCase(TestCase):
     """This class is used to test the Price model using django.test package"""
 
     def setUp(self):
+        self.now = timezone.now()
         self.item = Item.objects.create(name="Item", rarity="1", item_image="/static/images/item.png")
-        self.price = Price.objects.create(timestamp=datetime.now(), median_price=1234.0, lowest_price=123.0)
+        self.price = Price.objects.create(timestamp=self.now, median_price=1234.0, lowest_price=123.0)
 
     def test_price_with_item(self):
         self.price.item = 0
         self.assertTrue(isinstance(self.price, Price))
         self.assertTrue(isinstance(self.price.item, int))
-        self.assertTrue(isinstance(self.price.timestamp, datetime))
+        self.assertTrue(self.price.timestamp == self.now)
         self.assertTrue(isinstance(self.price.median_price, float))
         self.assertTrue(isinstance(self.price.lowest_price, float))
