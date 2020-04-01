ad221707bb1cd61afea71c0b9f12e4dd7e4d2c61
artan.sadiku <artan.sadiku@he-arc.ch>
2020-04-01 17:16:01 +0200

    Using default value if env. var is not set

--- CSRuby/wsgi.py
+++ CSRuby/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CSRuby.settings.development_settings')
+if os.environ.get('DJANGO_SETTINGS_MODULE') == None:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CSRuby.settings.development_settings')
 
 application = get_wsgi_application()
