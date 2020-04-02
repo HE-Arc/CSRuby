e5fc383b3015b6077b6eab463ee3ce94b7ed57b0
artan.sadiku <artan.sadiku@he-arc.ch>
2020-04-02 14:50:10 +0200

    Removed dashboard button in navbar

--- csruby_frontend_app/src/components/Main.js
+++ csruby_frontend_app/src/components/Main.js
@@ -39,16 +39,13 @@ class Main extends Component {
             <div>
               <header>
                 <nav className="navbar navbar-expand-lg navbar-dark csruby-bg-red">
-                  <NavLink className="navbar-brand" exact to="/">CSRuby</NavLink>
+                  <NavLink className="navbar-brand" exact to="/">CSRuby<span className="sr-only">(current)</span></NavLink>
                   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                   </button>
 
                   <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
-                      <li className="nav-item active">
-                        <NavLink className="nav-link" exact to="/">Dashboard<span className="sr-only">(current)</span></NavLink>
-                      </li>
                       <li className="nav-item active">
                         <NavLink className="nav-link" exact to="/search">Search</NavLink>
                       </li>
