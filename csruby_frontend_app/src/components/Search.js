e521f3a1f58b71de6f7ca0e26dfb8508a2a3906e
nicolas.minnig <nicolas.minnig@he-arc.ch>
2020-04-02 16:36:14 +0200

    added infinite scroll in search page

--- csruby_frontend_app/src/components/Search.js
+++ csruby_frontend_app/src/components/Search.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ItemPreview from './item/ItemPreview';
 import axios from 'axios';
+import InfiniteScroll from 'react-infinite-scroller';
 
 class Search extends Component {
   constructor(props) {
@@ -14,6 +15,9 @@ class Search extends Component {
       data: [],
       loaded: false,
       placeholder: 'Loading',
+      offset: 0,
+      limit_item: 10,
+      has_more_item: true,
     };
 
     this.onSubmit = this.onSubmit.bind(this);
@@ -25,40 +29,48 @@ class Search extends Component {
   }
 
   handleSearchChange(event) {
-    this.setState({search_value: event.target.value}, this.updateSearchResult);
+    this.setState({search_value: event.target.value, offset: 0, data:[]}, this.updateSearchResult);
   }
 
   handleRarityChange(event) {
-    this.setState({rarity: event.target.value}, this.updateSearchResult);
+    this.setState({rarity: event.target.value, offset: 0, data:[]}, this.updateSearchResult);
   }
 
   handleMinPriceChange(event) {
-    this.setState({min_price: event.target.value}, this.updateSearchResult);
+    this.setState({min_price: event.target.value, offset: 0, data:[]}, this.updateSearchResult);
   }
 
   handleMaxPriceChange(event) {
-    this.setState({max_price: event.target.value}, this.updateSearchResult);
+    this.setState({max_price: event.target.value, offset: 0, data:[]}, this.updateSearchResult);
   }
 
   handleOrderingChange(event) {
-    this.setState({ordering: event.target.value}, this.updateSearchResult);
+    this.setState({ordering: event.target.value, offset: 0, data:[]}, this.updateSearchResult);
   }
 
   updateSearchResult = () => {
-    var url = '/item/search?name=' + this.state.search_value + '&rarity=' + this.state.rarity + '&min_price=' + this.state.min_price + '&max_price=' + this.state.max_price + '&order_by=' + this.state.ordering;
+    var url = '/item/search?name=' + this.state.search_value + '&rarity=' + this.state.rarity + '&min_price=' + this.state.min_price + '&max_price=' + this.state.max_price + '&order_by=' + this.state.ordering + '&offset=' + this.state.offset;
     this.fetchItems(url);
   }
 
   onSubmit(event) {
-    var url = '/item/search?name=' + this.state.search_value + '&rarity=' + this.state.rarity + '&min_price=' + this.state.min_price + '&max_price=' + this.state.max_price + '&order_by=' + this.state.ordering;
+    console.log("OnSubmit");
+    var url = '/item/search?name=' + this.state.search_value + '&rarity=' + this.state.rarity + '&min_price=' + this.state.min_price + '&max_price=' + this.state.max_price + '&order_by=' + this.state.ordering + '&offset=' + this.state.offset;
     this.fetchItems(url);
     event.preventDefault();
   }
 
+  fetchMore(){
+    console.log("FETCHING MORE");
+    var url = '/item/search?name=' + this.state.search_value + '&rarity=' + this.state.rarity + '&min_price=' + this.state.min_price + '&max_price=' + this.state.max_price + '&order_by=' + this.state.ordering + '&offset=' + this.state.offset;
+    this.fetchItems(url);
+  }
+
   fetchItems(url) {
+    console.log("FETCHING");
     this.setState({
       loaded: false,
-      data: [],
+      has_more_item:false,
     });
 
     axios({
@@ -67,10 +79,14 @@ class Search extends Component {
     })
     .then((response) => {
       if(response.status === 200) {
+        var data = this.state.data;
+        response.data.map(item => data.push(item));
         this.setState({
-          data: response.data,
+          data: data,
           loaded: true,
-        })
+          offset: this.state.offset+this.state.limit_item,
+        });
+        this.setState({has_more_item:response.data.length==10});
       }
     })
     .catch((error) => {
@@ -84,7 +100,7 @@ class Search extends Component {
     this.fetchItems('/item/search');
   }
 
-  render() {
+  /*render() {
     return (
       <div className="container">
         <div className="csruby-bg-darkest p-4 mb-3">
@@ -143,6 +159,86 @@ class Search extends Component {
             <div className="text-center">
               <div class="loadingio-spinner-rolling-oq809e0ojtq">
                 <div class="ldio-5u0wj89ps2u">
+                  <div></div>
+                </div>
+              </div>
+            </div>
+          }
+        </div>
+      </div>
+    );
+  }*/
+
+  render(){
+    const loader = <div className="text-center"><div className="loadingio-spinner-rolling-oq809e0ojtq"><div className="ldio-5u0wj89ps2u"><div></div></div></div></div>;
+    var items = [];
+    this.state.data.map(item=>{
+      items.push(
+        <ItemPreview key={item.item_id} itemId={item.item_id} url={item.item_image} name={item.name} price={item.lowest_price} rarity_class={item.rarity}/>
+      )
+    });
+    return (
+      <div className="container">
+        <div className="csruby-bg-darkest p-4 mb-3">
+          <form onSubmit={this.onSubmit} ref="form">
+            <div className="form-row">
+              <div className="form-group col mb-0">
+                <input className="form-control" id="searchbar" type="text" placeholder="Search item..." name="search" value={this.state.search_value} onChange={this.handleSearchChange}/>
+              </div>
+              <div className="form-group col mb-0">
+                <select className="form-control" id="rarity" value={this.state.rarity} onChange={this.handleRarityChange}>
+                  <option value="">All rarities</option>
+                  <option value="COG">Consumer grade</option>
+                  <option value="ING">Industrial grade</option>
+                  <option value="MIS">Mil-spec</option>
+                  <option value="RST">Restricted</option>
+                  <option value="CLA">Classified</option>
+                  <option value="COV">Covert</option>
+                  <option value="EXR">Exceedingly Rare</option>
+                  <option value="CON">Contraband</option>
+                  <option value="RES">High Grade Sticker</option>
+                  <option value="HGS">Remarkable Sticker</option>
+                  <option value="EXS">Extraordinary Sticker</option>
+                  <option value="EXG">Extraordinary Gloves</option>
+                </select>
+              </div>
+              <div className="form-group col mb-0">
+                <input className="form-control" id="min_price" type="number" min="0" max="7500" value="0" name="min_price" value={this.state.min_price} onChange={this.handleMinPriceChange} placeholder="Min price..."/>
+              </div>
+              <div className="form-group col mb-0">
+                <input className="form-control" id="max_price" type="number" min="0" max="7500" value="7500" name="max_price" value={this.state.max_price} onChange={this.handleMaxPriceChange} placeholder="Max price..."/>
+              </div>
+              <div className="form-group col mb-0">
+                <select className="form-control" id="ordering" value={this.state.ordering} onChange={this.handleOrderingChange}>
+                  <option value="">None</option>
+                  <option value="price">Price ascending</option>
+                  <option value="price_reverse">Price descending</option>
+                  <option value="rarity">Rarity ascending</option>
+                  <option value="rarity_reverse">Rarity descending</option>
+                  <option value="name">Name A-Z</option>
+                  <option value="name_reverse">Name Z-A</option>
+                </select>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div className="result-container" id="result-container">
+          {this.state.data.length > 0
+            ? <InfiniteScroll
+                initialLoad={ false }
+                loadMore={this.fetchMore.bind(this)}
+                hasMore={this.state.has_more_item}
+                loader={loader}
+                useWindow={true}
+                getScrollParent={() => document.getElementById('result-container')}>
+                  {items}
+            </InfiniteScroll>
+            : <h3 className="text-dark text-center">{this.state.loaded == true && 'No items found...'}</h3>
+          }
+          {this.state.loaded == false &&
+            <div className="text-center">
+              <div className="loadingio-spinner-rolling-oq809e0ojtq">
+                <div className="ldio-5u0wj89ps2u">
                   <div></div>
                 </div>
               </div>
