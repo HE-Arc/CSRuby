82ae81165ad9cb73cd80a6e8f1f2345379b704dc
nicopraz <55592616+nicopraz@users.noreply.github.com>
2020-04-03 22:10:51 +0200

    Update Error.js
    Added text-danger class

--- csruby_frontend_app/src/components/Error.js
+++ csruby_frontend_app/src/components/Error.js
@@ -8,7 +8,7 @@ class Error extends Component {
         <div className="container">
             <div className="text-center">
                 <div>
-                    <h1 className="csruby-error-title">Oops!</h1>
+                    <h1 className="csruby-error-title text-danger">Oops!</h1>
                     <h2>{this.props.status}</h2>
                     <div>
                         Sorry, an error has occured. {this.props.detail}
