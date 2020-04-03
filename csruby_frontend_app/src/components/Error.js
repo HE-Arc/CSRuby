422ea40c6d404efbf047ef57876d2e420dd3e8e5
nicopraz <55592616+nicopraz@users.noreply.github.com>
2020-04-03 22:08:31 +0200

    Update Error.js

--- csruby_frontend_app/src/components/Error.js
+++ csruby_frontend_app/src/components/Error.js
@@ -5,22 +5,20 @@ import { Redirect, NavLink } from 'react-router-dom';
 class Error extends Component {
   render() {
     return(
-      <div className="content mt-5">
-          <div className="container mt-5">
-              <div className="text-center">
-                  <div>
-                      <h1 className="csruby-error-title">Oops!</h1>
-                      <h2>{this.props.status}</h2>
-                      <div>
-                          Sorry, an error has occured. {this.props.detail}
-                      </div>
-                      <div>
-                          <NavLink exact to="/">Take me Home</NavLink>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
+        <div className="container">
+            <div className="text-center">
+                <div>
+                    <h1 className="csruby-error-title">Oops!</h1>
+                    <h2>{this.props.status}</h2>
+                    <div>
+                        Sorry, an error has occured. {this.props.detail}
+                    </div>
+                    <div>
+                        <NavLink exact to="/">Take me Home</NavLink>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
   }
 }
