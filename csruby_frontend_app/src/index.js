a1fa56635ee3dc8c43204cc58785217f2669ff40
artan.sadiku <artan.sadiku@he-arc.ch>
2020-04-04 19:07:44 +0200

    Added chart.js as npm package

--- csruby_frontend_app/src/index.js
+++ csruby_frontend_app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Main from './components/Main';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Chart from 'chart.js';
 
 ReactDOM.render(
   <Main/>,
